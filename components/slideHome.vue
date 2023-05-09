cd11adec7d82e8257093d2cf89640426087c2c98
mahmoud.saadi <mahmoud.saadi@Andalusiagroup.net>
2023-05-09 14:29:31 +0300

    create slider in home page

--- components/slideHome.vue
+++ components/slideHome.vue
@@ -3,8 +3,16 @@
     <client-only>
       <splide class="z-20" :options="options">
         <splide-slide>
-            <img src="~assets/images/Mask Group 14874.png" alt="" srcset="">
+          <img src="~assets/images/Mask Group 14874.png" alt="" srcset="" />
         </splide-slide>
+        <splide-slide>
+          <img src="~assets/images/Mask Group 14874.png" alt="" srcset="" />
+        </splide-slide>
+        <splide-slide>
+          <img src="~assets/images/Mask Group 14874.png" alt="" srcset="" />
+        </splide-slide>
+
+        <splide-slide> <p>sadsadsad</p> </splide-slide>
       </splide>
     </client-only>
   </section>
@@ -12,17 +20,17 @@
 <script>
 export default {
   name: "SlideHome",
+
   data() {
     return {
       options: {
         rewind: false,
-        perPage: 3,
+        perPage: 2,
         arrows: true,
         gap: "1rem",
-        direction: this.$i18n.locale === "ar" ? "rtl" : "ltr",
         classes: {
           arrows:
-            "splide__arrows absolute -top-12 end-2 inline-block space-s-4",
+            "splide__arrows absolute -top-12 start-50 inline-block space-s-4",
           arrow:
             "inline-flex z-10 justify-center items-center p-2 w-8 h-8 rounded-full border-2 shadow-lg bg-[color:var(--accent-1)]) custom-slider-arrow-colors",
           prev: "splide__arrow--prev d-arrow-prev",
@@ -31,8 +39,9 @@ export default {
             "inline-flex absolute -bottom-8 left-[-41%] right-1/2 z-10 flex-wrap justify-center items-center p-0 m-0 w-11/12 translate-x-1/2",
           page: "inline-block bottom-0 p-0 m-1 w-2 h-2 lg:w-4 lg:h-4 rounded-full transition-all duration-200 custom-slider-bullet custom-is-active",
         },
+        direction: this.$i18n.locale === "ar" ? "rtl" : "ltr",
         breakpoints: {
-          992: {
+          3: {
             perPage: 1,
           },
         },
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [`~/assets/css/general.css`, `~/assets/css/tailwind.css`], // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/vue-splide.js"],
+  plugins: [
+    {
+      src: "~/plugins/vue-splide.js",
+      mode: "client",
+    },
+  ],
 
   loading: {
     color: "#1779BA",
