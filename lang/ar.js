97b54c008fce7ad8af59afedac000cb5a8b44a15
Mahmoud.Saadi@PC <Mahmoud.Saadi@Andalusiagroup.net>
2023-05-09 15:09:40 +0300

    dfdsfdsfdsfdsf

--- lang/ar.js
+++ lang/ar.js
@@ -3,4 +3,6 @@ export default {
   asd: "سيبيسبيسبيسب",
   sdssadsasadsadsadasddsaad: "سيبيسبيسبيسب",
   sdssadsadsaad: "سيبيسبيسبيسب",
+  sdssadsfsdfdsdsasadsadsadasddsaad: "سيبيسبيسبيسب",
+  sdssaddsfdsfssadsaad: "سيبيسبيسبيسب",
 };
