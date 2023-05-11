751ab146a066b8c0f863bf7a30cf7588b8f6d993
Mahmoud.Saadi@PC <Mahmoud.Saadi@Andalusiagroup.net>
2023-05-11 15:50:42 +0300

    sdff

--- components/slideHome.vue
+++ components/slideHome.vue
@@ -9,30 +9,45 @@
           <img src="~assets/images/Mask Group 14874.png" alt="" srcset="" />
         </splide-slide>
         <splide-slide>
-          <img src="~assets/images/Mask Group 14874.png" alt="" srcset="" />
+          <img :src="images" />
         </splide-slide>
-
         <splide-slide>
-          <p>{{ $t("sdsad") }}</p>
+          <div v-for="product in products" :key="product.id">
+            <p>{{ product.id }}</p>
+          </div>
         </splide-slide>
       </splide>
     </client-only>
-
     <div
-        v-if="loading"
-        class="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3"
+      v-if="loading"
+      class="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3"
+    >
+      <div
+        v-for="(item, index) in Array(skeletonLength)"
+        :key="`theSkeleton-${index}`"
+        class="flex overflow-hidden flex-col rounded-2xl border"
       >
-        <div
-          v-for="(item, index) in Array(skeletonLength)"
-          :key="`theSkeleton-${index}`"
-          class="flex overflow-hidden flex-col rounded-2xl border"
-        >
-          <TheSkeleton :width="1000" :height="190" />
-          <div class="p-3 space-y-3">
-            <div class="flex justify-between">
-              <TheSkeleton :width="200" :height="25" class="rounded-lg" />
-              <TheSkeleton :width="60" :height="60" class="ml-6" />
+        <TheSkeleton :width="1000" :height="190" />
+        <div class="p-3 space-y-3">
+          <div class="flex justify-between">
+            <TheSkeleton :width="200" :height="25" class="rounded-lg" />
+            <TheSkeleton :width="60" :height="60" class="ml-6" />
+          </div>
+          <div class="flex flex-row gap-2">
+            <div>
+              <TheSkeleton :width="25" :height="25" />
             </div>
+            <div>
+              <TheSkeleton :width="100" :height="25" class="rounded-lg" />
+            </div>
+          </div>
+          <div class="space-y-1">
+            <TheSkeleton :width="250" :height="5" />
+            <TheSkeleton :width="300" :height="5" class="rounded-lg" />
+            <TheSkeleton :width="300" :height="5" class="rounded-lg" />
+          </div>
+
+          <div class="flex flex-row justify-between">
             <div class="flex flex-row gap-2">
               <div>
                 <TheSkeleton :width="25" :height="25" />
@@ -41,48 +56,48 @@
                 <TheSkeleton :width="100" :height="25" class="rounded-lg" />
               </div>
             </div>
-            <div class="space-y-1">
-              <TheSkeleton :width="250" :height="5" />
-              <TheSkeleton :width="300" :height="5" class="rounded-lg" />
-              <TheSkeleton :width="300" :height="5" class="rounded-lg" />
-            </div>
 
-            <div class="flex flex-row justify-between">
-              <div class="flex flex-row gap-2">
-                <div>
-                  <TheSkeleton :width="25" :height="25" />
-                </div>
-                <div>
-                  <TheSkeleton :width="100" :height="25" class="rounded-lg" />
-                </div>
+            <div class="flex flex-row gap-2">
+              <div>
+                <TheSkeleton :width="25" :height="25" />
               </div>
-
-              <div class="flex flex-row gap-2">
-                <div>
-                  <TheSkeleton :width="25" :height="25" />
-                </div>
-                <div>
-                  <TheSkeleton
-                    :width="100"
-                    :height="25"
-                    class="ml-10 rounded-lg"
-                  />
-                </div>
+              <div>
+                <TheSkeleton
+                  :width="100"
+                  :height="25"
+                  class="ml-10 rounded-lg"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   </section>
 </template>
 <script>
 export default {
   name: "SlideHome",
+  mounted() {
+    this.blogList();
+  },
+  methods: {
+    blogList() {
+      this.$axios
+        .get("https://dummyjson.com/products")
+        .then((response) => {
+          this.products = response.data;
+        })
+        .catch()
+        .finally();
+    },
+  },
 
   data() {
     return {
       skeletonLength: 3,
       loading: true,
+      products: [],
       options: {
         rewind: false,
         perPage: 2,
